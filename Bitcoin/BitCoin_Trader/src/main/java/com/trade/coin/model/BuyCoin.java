51bb82ee859d106dff6052d68168f925977ec1a7
유한욱 <gi7182@naver.com>
2021-12-06 20:52:40 +0900

    Update BuyCoin.java

--- Bitcoin/BitCoin_Trader/src/main/java/com/trade/coin/model/BuyCoin.java
+++ Bitcoin/BitCoin_Trader/src/main/java/com/trade/coin/model/BuyCoin.java
@@ -24,52 +24,54 @@ import java.util.UUID;
 
 public class BuyCoin {
 	public void buy() throws NoSuchAlgorithmException, UnsupportedEncodingException, ParseException, org.json.simple.parser.ParseException {
+	
 		String accessKey = "ACCESS KEY";
-	    String secretKey = "SECRET KEY";
-	    String serverUrl = "https://api.upbit.com";
+		String secretKey = "SECRET KEY";
+		String serverUrl = "https://api.upbit.com";
 
-        HashMap<String, String> params = new HashMap<>();
-        params.put("market", "KRW-BTC");
-        params.put("side", "bid");
-        params.put("price", "5000");
-        params.put("ord_type", "price");
+		HashMap<String, String> params = new HashMap<>();
+		params.put("market", "KRW-BTC");
+		params.put("side", "bid");
+		params.put("price", "5000");
+		params.put("ord_type", "price");
 
-        ArrayList<String> queryElements = new ArrayList<>();
-        for(Map.Entry<String, String> entity : params.entrySet()) {
-            queryElements.add(entity.getKey() + "=" + entity.getValue());
-        }
+		ArrayList<String> queryElements = new ArrayList<>();
+		for(Map.Entry<String, String> entity : params.entrySet()) {
+		    queryElements.add(entity.getKey() + "=" + entity.getValue());
+		}
 
-        String queryString = String.join("&", queryElements.toArray(new String[0]));
+		String queryString = String.join("&", queryElements.toArray(new String[0]));
 
-        MessageDigest md = MessageDigest.getInstance("SHA-512");
-        md.update(queryString.getBytes("UTF-8"));
+		MessageDigest md = MessageDigest.getInstance("SHA-512");
+		md.update(queryString.getBytes("UTF-8"));
 
-        String queryHash = String.format("%0128x", new BigInteger(1, md.digest()));
+		String queryHash = String.format("%0128x", new BigInteger(1, md.digest()));
 
-        Algorithm algorithm = Algorithm.HMAC256(secretKey);
-        String jwtToken = JWT.create()
-                .withClaim("access_key", accessKey)
-                .withClaim("nonce", UUID.randomUUID().toString())
-                .withClaim("query_hash", queryHash)
-                .withClaim("query_hash_alg", "SHA512")
-                .sign(algorithm);
+		Algorithm algorithm = Algorithm.HMAC256(secretKey);
+		String jwtToken = JWT.create()
+			.withClaim("access_key", accessKey)
+			.withClaim("nonce", UUID.randomUUID().toString())
+			.withClaim("query_hash", queryHash)
+			.withClaim("query_hash_alg", "SHA512")
+			.sign(algorithm);
 
-        String authenticationToken = "Bearer " + jwtToken;
+		String authenticationToken = "Bearer " + jwtToken;
 
-        try {
-            HttpClient client = HttpClientBuilder.create().build();
-            HttpPost request = new HttpPost(serverUrl + "/v1/orders");
-            request.setHeader("Content-Type", "application/json");
-            request.addHeader("Authorization", authenticationToken);
-            request.setEntity(new StringEntity(new Gson().toJson(params)));
+		try {
+		    HttpClient client = HttpClientBuilder.create().build();
+		    HttpPost request = new HttpPost(serverUrl + "/v1/orders");
+		    request.setHeader("Content-Type", "application/json");
+		    request.addHeader("Authorization", authenticationToken);
+		    request.setEntity(new StringEntity(new Gson().toJson(params)));
 
-            HttpResponse response = client.execute(request);
-            HttpEntity entity = response.getEntity();
+		    HttpResponse response = client.execute(request);
+		    HttpEntity entity = response.getEntity();
 
-            System.out.println(EntityUtils.toString(entity, "UTF-8"));
-            System.out.println("매수 완료!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		    System.out.println(EntityUtils.toString(entity, "UTF-8"));
+		    System.out.println("매수 완료!");
+			
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
 	}
 }
