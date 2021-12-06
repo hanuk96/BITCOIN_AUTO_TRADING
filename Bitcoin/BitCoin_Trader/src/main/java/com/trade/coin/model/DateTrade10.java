05668576d58bf25b3d1bcea564a4624ada12ad58
유한욱 <gi7182@naver.com>
2021-12-06 20:56:06 +0900

    Update DateTrade10.java

--- Bitcoin/BitCoin_Trader/src/main/java/com/trade/coin/model/DateTrade10.java
+++ Bitcoin/BitCoin_Trader/src/main/java/com/trade/coin/model/DateTrade10.java
@@ -25,29 +25,30 @@ public class DateTrade10 {
 		Response response = client.newCall(request).execute();
 		String result = response.body().string();
 		JSONParser jsonParse = new JSONParser();
-	    JSONArray timeList = (JSONArray) jsonParse.parse(result);      
+	    	JSONArray timeList = (JSONArray) jsonParse.parse(result);      
        
-	    ArrayList<DateTradeDto> list = new ArrayList<DateTradeDto>();
-        for (int i = 0; i < timeList.size(); i++) {
-    	   JSONObject jsonObj = (JSONObject) timeList.get(i);
-    	   DateTradeDto date = new DateTradeDto();
-    	   date.setMarket(((String) jsonObj.get("market")));
-    	   date.setCandleDateTimeUtc(((String) jsonObj.get("candle_date_time_utc")));
-    	   date.setCandleDateTimeKst(((String) jsonObj.get("candle_date_time_kst")));//한국 기준
-    	   date.setOpen(((Double) jsonObj.get("opening_price")));//시가
-    	   date.setHigh(((Double) jsonObj.get("high_price")));//최고가
-    	   date.setLow(((Double) jsonObj.get("low_price")));//저가
-    	   date.setPrice(((Double) jsonObj.get("trade_price")));//종가
-    	   date.setVolume(((Double) jsonObj.get("candle_acc_trade_volume")));//누적 거래량
-    	   date.setPrevClosingPrice(((Double) jsonObj.get("prev_closing_price")));
-    	   list.add(date);
-        }
-        //10일간의 이동평균 계산
-        double target = 0.0;
-        for (int j = 0; j < 10; j++) {
-        	target += list.get(j).getPrice();
+	    	ArrayList<DateTradeDto> list = new ArrayList<DateTradeDto>();
+		for (int i = 0; i < timeList.size(); i++) {
+		   JSONObject jsonObj = (JSONObject) timeList.get(i);
+		   DateTradeDto date = new DateTradeDto();
+		   date.setMarket(((String) jsonObj.get("market")));
+		   date.setCandleDateTimeUtc(((String) jsonObj.get("candle_date_time_utc")));
+		   date.setCandleDateTimeKst(((String) jsonObj.get("candle_date_time_kst")));//한국 기준
+		   date.setOpen(((Double) jsonObj.get("opening_price")));//시가
+		   date.setHigh(((Double) jsonObj.get("high_price")));//최고가
+		   date.setLow(((Double) jsonObj.get("low_price")));//저가
+		   date.setPrice(((Double) jsonObj.get("trade_price")));//종가
+		   date.setVolume(((Double) jsonObj.get("candle_acc_trade_volume")));//누적 거래량
+		   date.setPrevClosingPrice(((Double) jsonObj.get("prev_closing_price")));
+		   list.add(date);
 		}
-        target /= 10;
-        return target;
+		//10일간의 이동평균 계산
+		double target = 0.0;
+		for (int j = 0; j < 10; j++) {
+			target += list.get(j).getPrice();
+		}
+		
+		target /= 10;
+		return target;
 	}
 }
